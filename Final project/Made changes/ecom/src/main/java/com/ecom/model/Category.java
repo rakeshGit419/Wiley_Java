5f496ad4b1b4c47b5693c9ea418e25c6b90e1cf5
rakeshGit419 <arigelarakesh04@gmail.com>
2022-04-28 19:07:21 +0530

    project

---
+++ Final project/Made changes/ecom/src/main/java/com/ecom/model/Category.java
@@ -0,0 +1,41 @@
+package com.ecom.model;
+
+
+import javax.persistence.*;
+
+
+@Entity
+public class Category {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "category_id")
+	
+	private int id;
+	private String name;
+	
+	public Category() {
+	}
+	public Category(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Category [id=" + id + ", name=" + name + "]";
+	}
+	
+	
+}
---
+++ Final project/Made changes/ecom/src/main/java/com/ecom/model/Role.java
@@ -0,0 +1,47 @@
+package com.ecom.model;
+
+import java.util.List;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	
+	@Column(nullable = false,unique = true)
+	@NotEmpty
+	private String name;
+	
+	@ManyToMany(mappedBy = "roles")
+	private List<User> users;
+	
+	public Role() {}
+
+	public Role(Integer id, @NotEmpty String name, List<User> users) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.users = users;
+	}
+
+	public Integer getId() {return id;}
+
+	public void setId(Integer id) {this.id = id;}
+
+	public String getName() {return name;}
+
+	public void setName(String name) {this.name = name;}
+
+	public List<User> getUsers() {return users;}
+
+	public void setUsers(List<User> users) {this.users = users;}
+
+	@Override
+	public String toString() {
+		return "Role [id=" + id + ", name=" + name + ", users=" + users + "]";
+	}
+}
